917b23832a6656d6f065b61ff70fa15e0374e55e
P3terHenry <pedro.antonieti@outlook.com>
2025-10-07 00:08:55 -0300

    :sparkles: Adicionado métodos de consulta personalizados nos repositórios Categoria, Lancamento e Usuario

--- src/main/java/br/com/fiap/devops/dimdim/repository/CategoriaRepository.java
+++ src/main/java/br/com/fiap/devops/dimdim/repository/CategoriaRepository.java
@@ -1,9 +1,17 @@
 package br.com.fiap.devops.dimdim.repository;
 
 import br.com.fiap.devops.dimdim.model.Categoria;
+import br.com.fiap.devops.dimdim.model.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    @Query("SELECT c FROM Categoria c WHERE c.usuarioCategoriaId = :id")
+    List<Categoria> findByUsuarioId(@Param("id") Long id);
 }
--- src/main/java/br/com/fiap/devops/dimdim/repository/LancamentoRepository.java
+++ src/main/java/br/com/fiap/devops/dimdim/repository/LancamentoRepository.java
@@ -1,9 +1,15 @@
 package br.com.fiap.devops.dimdim.repository;
 
+import br.com.fiap.devops.dimdim.model.Categoria;
 import br.com.fiap.devops.dimdim.model.Lancamento;
+import br.com.fiap.devops.dimdim.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    List<Lancamento> findByCategoriaLancamentoId(Categoria categoriaLancamentoId);
 }
